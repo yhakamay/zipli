9676ef97972d2b3ab4633e902642c79b6b20b0e0
Yusuke <yhakamaya@adobe.com>
2022-07-17 13:48:45 +0900

    Add Firebase to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'pages/home_page.dart';
 import 'pages/new_trip_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Tripper());
 }
 
